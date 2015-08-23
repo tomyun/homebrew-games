5e2f0a9ed9534ef38f3866b5c621ba51256fa3a8
Kyungdahm Yun <tomyun@gmail.com>
2015-08-22 19:02:01 -0700

    fuse-emulator-1.1.1 (new formula)
    Closes #325.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

---
+++ fuse-emulator.rb
@@ -0,0 +1,31 @@
+class FuseEmulator < Formula
+  desc "Free Unix Spectrum Emulator"
+  homepage "http://fuse-emulator.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/fuse-emulator/fuse/1.1.1/fuse-1.1.1.tar.gz"
+  sha256 "a857d4bd62e01d18429897467508af15624cbaf343caed8b9ba1ab3a8879444e"
+
+  head do
+    url "http://svn.code.sf.net/p/fuse-emulator/code/trunk/fuse"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "sdl"
+  depends_on "libspectrum"
+  depends_on "libpng"
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-sdltest",
+                          "--with-sdl",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/fuse", "--version"
+  end
+end
