dae9292691322c8c5af4187c09f5f3bae160d4cd
Kyungdahm Yun <tomyun@gmail.com>
2015-08-24 23:29:03 -0700

    stone-soup 0.16.2
    Closes #327.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- stone-soup.rb
+++ stone-soup.rb
@@ -1,9 +1,8 @@
-require 'formula'
-
 class StoneSoup < Formula
-  homepage 'http://crawl.develz.org/'
-  url 'https://downloads.sourceforge.net/project/crawl-ref/Stone%20Soup/0.15.2/stone_soup-0.15.2.tar.xz'
-  sha1 'fb9bec3f4adbb3e8cc60e6ddc77cd63f23654b81'
+  desc "Dungeon Crawl Stone Soup: a roguelike game"
+  homepage "https://crawl.develz.org/"
+  url "https://crawl.develz.org/release/stone_soup-0.16.2.tar.xz"
+  sha256 "49d5947abb341eb7f0590fa538c475e68ea954064c615d852de968211b68780b"
 
   bottle do
     sha1 "ffda406e7326f61980800f2c09686cc6f817606d" => :yosemite
@@ -11,11 +10,47 @@ class StoneSoup < Formula
     sha1 "9c9f1b441fb8972b65dc8d2d48db52ade96b9f3e" => :mountain_lion
   end
 
+  option "with-tiles", "Enable graphic tiles and sound"
+
+  depends_on "pkg-config" => :build
+  depends_on "lua51"
+  depends_on "pcre"
+
+  if build.with? "tiles"
+    depends_on "sdl2"
+    depends_on "sdl2_mixer"
+    depends_on "sdl2_image"
+    depends_on "libpng"
+    depends_on "freetype"
+  end
+
+  needs :cxx11
+
   def install
-    # Numerous template issues building under libc++
-    ENV.libstdcxx
+    ENV.cxx11
 
     cd "source" do
+      args = %W[
+        prefix=#{prefix}
+        DATADIR=data
+        NO_PKGCONFIG=
+        BUILD_ZLIB=
+        BUILD_LUA=
+        BUILD_SQLITE=
+        BUILD_FREETYPE=
+        BUILD_LIBPNG=
+        BUILD_SDL2=
+        BUILD_SDL2MIXER=
+        BUILD_SDL2IMAGE=
+        BUILD_PCRE=
+        USE_PCRE=y
+      ]
+      if build.with? "tiles"
+        inreplace "Makefile", "contrib/install/$(ARCH)/lib/libSDL2main.a", ""
+        args << "TILES=y"
+        args << "SOUND=y"
+      end
+
       # The makefile has trouble locating the developer tools for
       # CLT-only systems, so we set these manually. Reported upstream:
       # https://crawl.develz.org/mantis/view.php?id=7625
@@ -23,13 +58,18 @@ class StoneSoup < Formula
       # On 10.9, stone-soup will try to use xcrun and fail due to an empty
       # DEVELOPER_DIR
       devdir = MacOS::Xcode.prefix.to_s
-      devdir += '/' if MacOS.version >= :mavericks && !MacOS::Xcode.installed?
+      devdir += "/" if MacOS.version >= :mavericks && !MacOS::Xcode.installed?
 
-      system "make", "install", "prefix=#{prefix}", "DATADIR=data/",
+      system "make", "install",
         "DEVELOPER_DIR=#{devdir}", "SDKROOT=#{MacOS.sdk_path}",
         # stone-soup tries to use `uname -m` to determine build -arch,
         # which is frequently wrong on OS X
-        "SDK_VER=#{MacOS.version}", "MARCH=#{MacOS.preferred_arch}"
+        "SDK_VER=#{MacOS.version}", "MARCH=#{MacOS.preferred_arch}",
+        *args
     end
+  end
+
+  test do
+    assert shell_output("#{bin}/crawl --version").start_with? "Crawl version #{version}"
   end
 end
