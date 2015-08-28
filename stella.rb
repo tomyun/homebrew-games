7c59332785af69dc7a788b212dfc94a44d0a92ca
Kyungdahm Yun <tomyun@gmail.com>
2015-08-28 16:10:21 -0700

    stella 4.6.1 (new formula)
    Closes #333.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

---
+++ stella.rb
@@ -0,0 +1,30 @@
+class Stella < Formula
+  desc "Atari 2600 VCS emulator"
+  homepage "http://stella.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/stella/stella/4.6.1/stella-4.6.1-src.tar.gz"
+  sha256 "c1921671dbc08422ae8a7a4102c6a2a34433f04594d67f55a7129c1504bcd288"
+  head "http://svn.code.sf.net/p/stella/code/trunk"
+
+  depends_on :xcode => :build
+  depends_on "sdl2"
+  depends_on "libpng"
+
+  def install
+    cd "src/macosx" do
+      inreplace "stella.xcodeproj/project.pbxproj" do |s|
+        s.gsub! %r[(\w{24} /\* SDL2\.framework)], '//\1'
+        s.gsub! %r[(\w{24} /\* png)], '//\1'
+        s.gsub! /(HEADER_SEARCH_PATHS) = \(/, "\\1 = (#{Formula["sdl2"].include}/SDL2, #{Formula["libpng"].include},"
+        s.gsub! /(LIBRARY_SEARCH_PATHS) = \.;/, "\\1 = (#{Formula["sdl2"].lib}, #{Formula["libpng"].lib}, .);"
+        s.gsub! /(OTHER_LDFLAGS) = "((-\w+)*)"/, '\1 = "-lSDL2 -lpng \2"'
+      end
+      xcodebuild "SYMROOT=build"
+      prefix.install "build/Default/Stella.app"
+      bin.write_exec_script "#{prefix}/Stella.app/Contents/MacOS/Stella"
+    end
+  end
+
+  test do
+    assert_match /Stella version #{version}/, shell_output("#{bin}/Stella -help").strip
+  end
+end
