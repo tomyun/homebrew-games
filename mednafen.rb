cb2448e6b2a58f761fb937831e57aae0d5644a1b
Kyungdahm Yun <tomyun@gmail.com>
2015-08-28 14:46:32 -0700

    mednafen 0.9.38.6
    Closes #337.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- mednafen.rb
+++ mednafen.rb
@@ -1,9 +1,8 @@
-require 'formula'
-
 class Mednafen < Formula
-  homepage 'http://mednafen.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/mednafen/Mednafen/0.9.36.5/mednafen-0.9.36.5.tar.bz2'
-  sha1 '02d441e18083daa539f0193121ac760882be9d19'
+  desc "Multi-system emulator"
+  homepage "http://mednafen.fobby.net/"
+  url "http://mednafen.fobby.net/releases/files/mednafen-0.9.38.6.tar.bz2"
+  sha256 "9460da3c6cd8cb8a02293d51c958cbc6ab5555aa6d115b952d4db2e0f1067e47"
 
   bottle do
     sha1 "84e3203ab953fbed5a22499a2ad64aa6f17f4d80" => :yosemite
@@ -11,13 +10,28 @@ class Mednafen < Formula
     sha1 "09ade315444535e750cb204d0280c9c7d833f12a" => :mountain_lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'sdl'
-  depends_on 'libcdio'
-  depends_on 'libsndfile'
+  depends_on "pkg-config" => :build
+  depends_on "sdl"
+  depends_on "libsndfile"
+  depends_on "gettext"
+
+  needs :cxx11
+
+  fails_with :clang do
+    build 602
+    cause <<-EOS.undent
+      LLVM miscompiles some loop code with optimization
+      https://llvm.org/bugs/show_bug.cgi?id=15470
+      EOS
+  end
 
   def install
+    ENV.cxx11
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_equal "#{version}", shell_output("#{bin}/mednafen -dump_modules_def M >/dev/null || head -n 1 M").chomp
   end
 end
