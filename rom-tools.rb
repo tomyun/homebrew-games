1cd1918f9d8e5b747b8860fda9ee167c8e4bf2e3
Kyungdahm Yun <tomyun@gmail.com>
2015-08-30 13:23:39 -0700

    rom-tools 0.165
    Closes #341.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- rom-tools.rb
+++ rom-tools.rb
@@ -1,9 +1,9 @@
 class RomTools < Formula
   desc "Tools for Multiple Arcade Machine Emulator"
   homepage "http://mamedev.org/"
-  url "https://github.com/mamedev/mame/archive/mame0164.tar.gz"
-  version "0.164"
-  sha256 "01e9b3c03c7a379a0aecea50415c9ba687d8f0af282a8447027d02d279055a81"
+  url "https://github.com/mamedev/mame/archive/mame0165.tar.gz"
+  version "0.165"
+  sha256 "00959b21d949685106528af7d68b92d8ba51ace72651ad582c2fb033ed77292e"
   head "https://github.com/mamedev/mame.git"
 
   bottle do
@@ -14,27 +14,52 @@ class RomTools < Formula
   end
 
   depends_on "sdl2"
+  depends_on "flac"
+  depends_on "sqlite"
+  depends_on "portmidi"
+
+  # FIXME: prevent build failure, fixed upstream
+  # https://github.com/mamedev/mame/pull/292
+  patch do
+    url "https://github.com/mamedev/mame/commit/2afb39374410d803dd287f83de44fbf7a4c2edc8.diff"
+    sha256 "0419953fc46170e2e114fbd369cfa461894b30a8c54c4729917b31575ed078a8"
+  end
 
   def install
-    ENV["MACOSX_USE_LIBSDL"] = "1"
-    ENV["PTR64"] = (MacOS.prefer_64_bit? ? "1" : "0")
-
-    system "make", "CC=#{ENV.cc}", "LD=#{ENV.cxx}", "TARGET=ldplayer", "TOOLS=1"
-
+    inreplace "scripts/src/main.lua", /(targetsuffix) "\w+"/, '\1 ""'
+    system "make", "TARGET=ldplayer", "TOOLS=1",
+                   "PTR64=#{MacOS.prefer_64_bit? ? 1 : 0}", # for old Macs
+                   "MACOSX_USE_LIBSDL=1",
+                   "USE_SYSTEM_LIB_ZLIB=1",
+                   "USE_SYSTEM_LIB_FLAC=1",
+                   "USE_SYSTEM_LIB_PORTMIDI=1"
     bin.install %W[
-      castool chdman floptool imgtool jedutil ldresample ldverify nltool pngcmp
+      castool chdman floptool imgtool jedutil ldplayer ldresample ldverify nltool nlwav pngcmp
       regrep romcmp src2html srcclean testkeys unidasm
     ]
     bin.install "split" => "rom-split"
-    if MacOS.prefer_64_bit?
-      bin.install "ldplayer64" => "ldplayer"
-    else
-      bin.install "ldplayer"
-    end
     man1.install Dir["src/osd/sdl/man/*.1"]
   end
 
+  # Needs more comprehensive tests
   test do
+    system "#{bin}/castool"
+    # system "#{bin}/chdman"
+    system "#{bin}/floptool"
+    system "#{bin}/imgtool", "listformats"
+    system "#{bin}/jedutil", "-viewlist"
     system "#{bin}/ldplayer", "-help"
+    # system "#{bin}/ldresample"
+    # system "#{bin}/ldverify"
+    # system "#{bin}/nltool", "--help" # segmentation fault
+    # system "#{bin}/nlwav", "--help" # segmentation fault
+    # system "#{bin}/pngcmp"
+    # system "#{bin}/regrep"
+    system "#{bin}/romcmp"
+    system "#{bin}/rom-split"
+    # system "#{bin}/src2html"
+    # system "#{bin}/srcclean"
+    # system "#{bin}/testkeys"
+    # system "#{bin}/unidasm"
   end
 end
