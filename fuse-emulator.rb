d73c916d3cb13b3f0af8ef99e9387fd37e58a273
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-31 03:27:57 +0100

    fuse-emulator: add 1.1.1 bottle.

--- fuse-emulator.rb
+++ fuse-emulator.rb
@@ -4,6 +4,12 @@ class FuseEmulator < Formula
   url "https://downloads.sourceforge.net/project/fuse-emulator/fuse/1.1.1/fuse-1.1.1.tar.gz"
   sha256 "a857d4bd62e01d18429897467508af15624cbaf343caed8b9ba1ab3a8879444e"
 
+  bottle do
+    sha256 "d8ec4e9ca09fc74f3a0f31ae8a0c627d8cefc4b1e36fb05fb6254889dde1502c" => :yosemite
+    sha256 "26374b7ab68ca721cb35f76c62258903fdc60e4b655f613e3e7167af10d77798" => :mavericks
+    sha256 "c370112cf115f50113fa95fb39675e6b05231c9985b5bfc1f00a0c5f5c152ec5" => :mountain_lion
+  end
+
   head do
     url "http://svn.code.sf.net/p/fuse-emulator/code/trunk/fuse"
     depends_on "automake" => :build
