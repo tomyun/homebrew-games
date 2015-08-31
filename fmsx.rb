353a847c777b3cab88dfc8a28c7e46f5814459a9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-31 05:17:04 +0100

    fmsx: add 4.2 bottle.

--- fmsx.rb
+++ fmsx.rb
@@ -5,6 +5,13 @@ class Fmsx < Formula
   version "4.2"
   sha256 "7040bd61b309d4b514c20c7413d48f9b816e2f91e8bf7bb268d6143cbaa75e00"
 
+  bottle do
+    cellar :any
+    sha256 "1d52b14f7b11e4d4ddf33e31e7362414aebfdbed0d881e9aadd6a23bc58f3604" => :yosemite
+    sha256 "b5098885668691d4105897d7b3602ab320498682c98a49ce3894544a91490f77" => :mavericks
+    sha256 "af8f080ef802c0609adacf9c3c9bc3b7c11cbebd469e833204ce59c3960f480e" => :mountain_lion
+  end
+
   depends_on :x11
   depends_on "pulseaudio"
 
