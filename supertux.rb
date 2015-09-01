d343fe69b53b2c1baaac5d22c16f87557626f34b
Kyungdahm Yun <tomyun@gmail.com>
2015-09-01 16:16:48 -0700

    supertux 0.3.5a (devel)
    Closes #342.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- supertux.rb
+++ supertux.rb
@@ -1,71 +1,61 @@
-require 'formula'
-
 class Supertux < Formula
-  homepage 'http://supertux.lethargik.org/'
-  url 'https://downloads.sf.net/project/supertux.berlios/supertux-0.1.3.tar.bz2'
-  sha1 'ad783011fbf15a8a56ca8680769a8c2a15abc515'
-
-  depends_on 'sdl'
-  depends_on 'sdl_image'
-  depends_on 'sdl_mixer'
-  depends_on 'physfs'
-  depends_on 'libogg'
-  depends_on 'libvorbis'
-
-  devel do
-    url 'https://supertux.googlecode.com/files/supertux-0.3.4.tar.bz2'
-    sha1 '9502cdad9ab8d04074ed57a6568fc87c7e857dcb'
-
-    depends_on 'cmake' => :build
-    depends_on 'glew'
-    depends_on 'boost'
-
-    fails_with :clang do
-      cause <<-EOS.undent
-        error: implicit instantiation of undefined template 'std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char> >'
-        std::string message;
-        EOS
-    end
-  end
+  desc "Classic 2D jump'n run sidescroller game"
+  homepage "https://supertux.github.io/"
 
   stable do
-    # Patch from macports port
-    # https://trac.macports.org/ticket/29635
-    patch :p0, :DATA
+    url "https://github.com/SuperTux/supertux/releases/download/v0.1.3/supertux-0.1.3.tar.bz2"
+    sha256 "0092588351776626955339caaa62d12ce5954bb80c5f6952f60a122f53c2ad97"
+
+    depends_on "sdl"
+    depends_on "sdl_image"
+    depends_on "sdl_mixer" => "with-libvorbis"
+  end
+
+  devel do
+    url "https://github.com/SuperTux/supertux/releases/download/v0.3.5a/supertux-0.3.5a.tar.bz2"
+    sha256 "71c34502b5879a4130fb111ac754967c3a632ef3017f30883a4d496d87bef8c7"
+
+    depends_on "cmake" => :build
+    depends_on "pkg-config" => :build
+    depends_on "boost" => :build
+    depends_on "sdl2"
+    depends_on "sdl2_image"
+    depends_on "sdl2_mixer" => "with-libvorbis"
+    depends_on "libogg"
+    depends_on "libvorbis"
+    depends_on "physfs"
+    depends_on "glew"
+
+    needs :cxx11
   end
 
   def install
-    if build.devel?
-      args = std_cmake_args
-      args << '-DINSTALL_SUBDIR_BIN=bin' << '-DINSTALL_SUBDIR_SHARE=share/supertux2'
-      system "cmake", ".", *args
-      system "make"
-      bin.install ['supertux2']
-      share.mkpath
-      (share + 'supertux2').mkpath
-      (share + 'supertux2').install Dir['data/*']
-    else
-      system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    if build.stable?
+      # https://trac.macports.org/ticket/29635
+      inreplace "src/menu.h", /Menu::(get_controlfield_key_into_input)/, '\1'
+      system "./configure", "--disable-dependency-tracking",
+                            "--with-apple-opengl-framework",
                             "--prefix=#{prefix}"
-      system "make install"
+      system "make", "install"
+      (share/"applications").rmtree
+      (share/"pixmaps").rmtree
+    else
+      ENV.cxx11
+      # Prevent build failure on Mavericks
+      inreplace "src/supertux/screen_manager.hpp", /.*\(Action &a\)[^}]+}/, ""
+      system "cmake", ".", *std_cmake_args
+      system "make", "install"
+      bin.write_exec_script "#{prefix}/SuperTux.app/Contents/MacOS/supertux2"
+      (share/"appdata").rmtree
     end
   end
 
   test do
-    system "supertux"
+    if stable?
+      (testpath/".supertux/config").write ""
+      assert_match /SuperTux #{version}/, shell_output("#{bin}/supertux --version")
+    else
+      assert_equal "supertux2 #{version}", shell_output("#{bin}/supertux2 --version").chomp
+    end
   end
 end
-
-__END__
---- src/menu.h.orig	2005-06-21 16:16:07.000000000 -0500
-+++ src/menu.h	2011-08-29 03:28:39.000000000 -0500
-@@ -207,7 +207,7 @@
- 
-   bool isToggled(int id);
- 
--  void Menu::get_controlfield_key_into_input(MenuItem *item);
-+  void get_controlfield_key_into_input(MenuItem *item);
- 
-   void draw   ();
-   void draw_item(int index, int menu_width, int menu_height);
-
