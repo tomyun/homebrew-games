c1af90544dea23ee8cb216057ede81a57dcaddc6
Kyungdahm Yun <tomyun@gmail.com>
2015-09-04 22:57:02 -0700

    roll: add HEAD
    Closes #349.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- roll.rb
+++ roll.rb
@@ -1,14 +1,19 @@
-require 'formula'
-
 class Roll < Formula
-  homepage 'http://matteocorti.ch/software/roll.html'
-  url 'http://matteocorti.ch/software/roll/roll-2.0.2.tar.gz'
-  sha1 '27899218903b6f2f435990f6aebdefe753f3a28a'
+  desc "CLI program for rolling a dice sequence"
+  homepage "http://matteocorti.ch/software/roll.html"
+  url "http://matteocorti.ch/software/roll/roll-2.0.2.tar.gz"
+  sha256 "15b3fb5fe43c61799da3e15574aec4fb414e30e7bfda3b1e4a3b4b398a3a99a9"
+  head do
+    url "svn://svn.code.sf.net/p/roll/code/trunk/roll"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
 
   def install
+    system "./regen.sh" if build.head?
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
   test do
