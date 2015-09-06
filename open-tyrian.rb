c3e30654a873546efa582588002908a101d94291
Kyungdahm Yun <tomyun@gmail.com>
2015-09-06 00:34:41 -0700

    open-tyrian: add HEAD, update URLs
    Closes #359.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- open-tyrian.rb
+++ open-tyrian.rb
@@ -1,28 +1,36 @@
 class OpenTyrian < Formula
-  homepage "https://code.google.com/p/opentyrian/"
+  desc "Open-source port of Tyrian"
+  homepage "https://bitbucket.org/opentyrian/opentyrian"
   url "http://www.camanis.net/opentyrian/releases/opentyrian-2.1.20130907-src.tar.gz"
-  sha1 "c5b97aea3931db6889acb639e59e619162b05183"
+  sha256 "f54b6b3cedcefa187c9f605d6164aae29ec46a731a6df30d351af4c008dee45f"
+  head "https://bitbucket.org/opentyrian/opentyrian", :using => :hg
 
   depends_on "sdl"
   depends_on "sdl_net"
 
   resource "data" do
-    url "http://sites.google.com/a/camanis.net/opentyrian/tyrian/tyrian21.zip"
-    sha1 "29827de99c92cdba7ac8d8b1307776c8f473cd44"
+    url "http://camanis.net/tyrian/tyrian21.zip"
+    sha256 "7790d09a2a3addcd33c66ef063d5900eb81cc9c342f4807eb8356364dd1d9277"
   end
 
   def install
-    libexec.install resource("data")
-
-    system "make release"
-    libexec.install "opentyrian"
-    (bin/"opentyrian").write <<-END.undent
-      #!/bin/bash
-      "#{libexec}/opentyrian" --data="#{libexec}" "$@"
-    END
+    datadir = pkgshare/"data"
+    datadir.install resource("data")
+    args = []
+    if build.head?
+      args << "TYRIAN_DIR=#{datadir}"
+    else
+      inreplace "src/file.c", "/usr/share/opentyrian/data", datadir
+    end
+    system "make", *args
+    bin.install "opentyrian"
   end
 
   def caveats
     "Save games will be put in ~/.opentyrian"
+  end
+
+  test do
+    system "#{bin}/opentyrian", "--help"
   end
 end
