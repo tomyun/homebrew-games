947a9aa594bf2011481aca82386f8dce5a2c3c22
Kyungdahm Yun <tomyun@gmail.com>
2015-09-07 03:10:26 -0700

    go-gui: modernize
    Closes #364.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- go-gui.rb
+++ go-gui.rb
@@ -1,22 +1,47 @@
-require 'formula'
-
 class GoGui < Formula
-  homepage 'http://gogui.sourceforge.net'
-  url 'https://downloads.sourceforge.net/project/gogui/gogui/1.4.9/gogui-1.4.9.zip'
-  sha1 'b4c402fa42ea054e251642e56a5da0be890c24e5'
+  desc "GUI for playing Go over Go Text Protocol"
+  homepage "http://gogui.sourceforge.net"
+  url "https://downloads.sourceforge.net/project/gogui/gogui/1.4.9/gogui-1.4.9.zip"
+  sha256 "32684b756ab5b6bf9412c035594eddfd1be9250de12d348c3501850857b86662"
 
-  depends_on :ant
+  head do
+    url "git://git.code.sf.net/p/gogui/code"
+
+    depends_on "docbook" => :build
+    depends_on "docbook-xsl" => :build
+  end
+
+  depends_on :ant => :build
+  depends_on :java => "1.6"
+
+  resource "quaqua" do
+    url "http://www.randelshofer.ch/quaqua/files/quaqua-5.4.1.nested.zip"
+    sha256 "a01ce8bcce6e81941ca928468e728e76e0773957c685c349474ee04f3be677d6"
+  end
 
   def install
     inreplace "build.xml", "/Developer/Tools/SetFile", "/usr/bin/SetFile"
-    system "ant", "gogui.app", "-Ddoc-uptodate=true"
-    prefix.install 'build/GoGui.app'
+    if build.head?
+      resource("quaqua").stage do
+        system "unzip", "quaqua-*.zip"
+        (buildpath/"lib").install "Quaqua/dist/quaqua.jar"
+      end
+      args = %W[
+        -Ddocbook-xsl.dir=#{Formula["docbook-xsl"].prefix}/docbook-xsl
+        -Ddocbook.dtd-4.2=#{Formula["docbook"].prefix}/docbook/xml/4.2
+      ]
+    else
+      args = %W[
+        -Ddoc-uptodate=true
+      ]
+    end
+    system "ant", "gogui.app", *args
+    prefix.install "build/GoGui.app"
+    bin.write_exec_script "#{prefix}/GoGui.app/Contents/MacOS/JavaApplicationStub"
+    mv "#{bin}/JavaApplicationStub", "#{bin}/gogui"
   end
 
-  def caveats; <<-EOS.undent
-    GoGui.app installed to:
-      #{prefix}
-    Use `brew linkapps` to symlink into ~/Applications.
-    EOS
+  test do
+    assert_equal "GoGui #{version}", shell_output("#{bin}/gogui -version").chomp
   end
 end
