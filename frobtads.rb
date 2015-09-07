ce84d98e4e8652be8317115c315da8aa3625493b
Kyungdahm Yun <tomyun@gmail.com>
2015-09-07 02:47:48 -0700

    frobtads: modernize
    Closes #362.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- frobtads.rb
+++ frobtads.rb
@@ -1,12 +1,11 @@
-require 'formula'
-
 class Frobtads < Formula
-  homepage 'http://www.tads.org/frobtads.htm'
-  url 'http://www.tads.org/frobtads/frobtads-1.2.3.tar.gz'
-  sha1 '330217c0b6ee298bf9db986bc4ce84b86aa5e3e2'
+  desc "TADS interpreter and compilers"
+  homepage "http://www.tads.org/frobtads.htm"
+  url "http://www.tads.org/frobtads/frobtads-1.2.3.tar.gz"
+  sha256 "88c6a987813d4be1420a1c697e99ecef4fa9dd9bc922be4acf5a3054967ee788"
 
   head do
-    url 'https://github.com/realnc/frobtads.git'
+    url "https://github.com/realnc/frobtads.git"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
@@ -17,6 +16,10 @@ class Frobtads < Formula
     system "./bootstrap" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_match /FrobTADS #{version}$/, shell_output("#{bin}/frob --version")
   end
 end
