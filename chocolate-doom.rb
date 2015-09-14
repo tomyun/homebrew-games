bd3cf690d249c0e65442ddd923e9bb77efcb77b9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-14 18:40:15 +0100

    chocolate-doom: add 2.2.1 bottle.

--- chocolate-doom.rb
+++ chocolate-doom.rb
@@ -5,6 +5,13 @@ class ChocolateDoom < Formula
   url "http://www.chocolate-doom.org/downloads/2.2.1/chocolate-doom-2.2.1.tar.gz"
   sha256 "ad11e2871667c6fa0658abf2dcba0cd9b26fbd651ee8df55adfdc18ad8fd674a"
 
+  bottle do
+    cellar :any
+    sha256 "5567c379e22722310d5e818e6730c482258ce550cb8bce684ed4d32b5f47ad17" => :yosemite
+    sha256 "7b364c31c685ffe93721b0bb4e5bbe994ce9d4e9446fd7705b34c7dc7b812a0d" => :mavericks
+    sha256 "394c2b0a47e3f71fa13b21669e1bff2b5a850abf054fcc37c0ac8e849e9e2c93" => :mountain_lion
+  end
+
   depends_on "sdl"
   depends_on "sdl_net"
   depends_on "sdl_mixer"
