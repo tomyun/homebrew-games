092f83f05c555d162608fbc30fa5d5d5368e33fe
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-21 00:50:01 +0100

    fs-uae: add 2.6.1 bottle.

--- fs-uae.rb
+++ fs-uae.rb
@@ -7,6 +7,13 @@ class FsUae < Formula
     sha256 "e5506f18017449fcc1e63e1186720c10beeff0fac44fb53cb08c4fb3bca74ba7"
   end
 
+  bottle do
+    cellar :any
+    sha256 "da99ed33136b7933ebf502f6d8c5c27ff3e58e196540ffa58d83c9c3a0d77cf4" => :el_capitan
+    sha256 "a37024708147145117bea38119736ad49302fce8a2840f9cc6d37946e4e08673" => :yosemite
+    sha256 "6fd5ccb0e18f51578b192ebfd7bbba75d74088b7c7e55645bdc0a4109ae7623b" => :mavericks
+  end
+
   devel do
     url "http://fs-uae.net/fs-uae/devel/2.7.0dev/fs-uae-2.7.0dev.tar.gz"
     version "2.7.0dev"
