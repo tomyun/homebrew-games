4b8df1c591db92fad62d94038c45cb51e0e17354
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-22 10:34:38 +0100

    gnu-go: add 3.8 bottle.

--- gnu-go.rb
+++ gnu-go.rb
@@ -6,6 +6,13 @@ class GnuGo < Formula
   sha256 "da68d7a65f44dcf6ce6e4e630b6f6dd9897249d34425920bfdd4e07ff1866a72"
   head "git://git.savannah.gnu.org/gnugo.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "57ccdefc160d72addd7bcad4d4254671193329f87925d514968056922faa305a" => :el_capitan
+    sha256 "7948fd7279d67d8894c6abf1ba9b0dc56bae40be2b6084fad703dc62d70bc5ca" => :yosemite
+    sha256 "b62358ec7d3aad424e073fa4db20a69c03c5f5629b9467af420f76a5fdca1c75" => :mavericks
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
