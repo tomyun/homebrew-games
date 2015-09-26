d5cf551eb9b5e9646dffff4b8ab45744fbafe2e7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-26 20:06:58 +0100

    solarus: add 1.4.4 bottle.

--- solarus.rb
+++ solarus.rb
@@ -5,6 +5,13 @@ class Solarus < Formula
   sha256 "ed27330952a2413f017deba3b563e8cd45ea29059ebcdb0ebcdff0f779d87709"
   head "https://github.com/christopho/solarus.git"
 
+  bottle do
+    cellar :any
+    sha256 "20cb99b7e755776e5d41f9c3a8fe32e421a22d47037d9866bcefab97bcc1a914" => :el_capitan
+    sha256 "7748d677b23fc236c8fd48e56ef041f13ce18a82144ab581452718d0420784a0" => :yosemite
+    sha256 "d883c70103feba9416b2493929bdbc4e323102b63f600fe84a86234a06bb4805" => :mavericks
+  end
+
   depends_on "cmake" => :build
   depends_on "sdl2"
   depends_on "sdl2_image"
