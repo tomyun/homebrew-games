65db0c19c61992ad73abebcca33b5a4411f720ac
Kyungdahm Yun <tomyun@gmail.com>
2015-09-28 10:57:53 -0700

    pioneer 20150927 (new formula)
    Closes #411.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

---
+++ pioneer.rb
@@ -0,0 +1,37 @@
+class Pioneer < Formula
+  desc "Game of lonely space adventure"
+  homepage "http://pioneerspacesim.net/"
+  url "https://github.com/pioneerspacesim/pioneer/archive/20150927.tar.gz"
+  sha256 "b68f05dc3bb10106700108d3d61afd942daade4a1e50d1a102326a91e00488d0"
+  head "https://github.com/pioneerspacesim/pioneer.git"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "assimp"
+  depends_on "freetype"
+  depends_on "sdl2"
+  depends_on "sdl2_image"
+  depends_on "libsigc++"
+  depends_on "libvorbis"
+  depends_on "libpng"
+  depends_on "lua"
+
+  needs :cxx11
+
+  def install
+    ENV.cxx11
+    ENV["PIONEER_DATA_DIR"] = "#{pkgshare}/data"
+    system "./bootstrap"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--with-version=#{version}",
+                          "--with-external-liblua"
+    system "make", "install"
+  end
+
+  test do
+    assert_equal "#{name} #{version}", shell_output("#{bin}/pioneer -v 2>&1").chomp
+  end
+end
