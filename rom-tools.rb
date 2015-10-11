70a0a64efdc2b4989bead8ce3189cbf515312d5c
Parry Chou <mbcoguno@users.noreply.github.com>
2015-10-11 12:13:54 +0800

    rom-tools 0.166
    * Add OS X only tool
    
    Closes #424.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- rom-tools.rb
+++ rom-tools.rb
@@ -1,9 +1,9 @@
 class RomTools < Formula
   desc "Tools for Multiple Arcade Machine Emulator"
   homepage "http://mamedev.org/"
-  url "https://github.com/mamedev/mame/archive/mame0165.tar.gz"
-  version "0.165"
-  sha256 "00959b21d949685106528af7d68b92d8ba51ace72651ad582c2fb033ed77292e"
+  url "https://github.com/mamedev/mame/archive/mame0166.tar.gz"
+  version "0.166"
+  sha256 "702a2019a05afd7f050db6b38c3aaee4cc46ec2214cecaed3420889a685637f4"
   head "https://github.com/mamedev/mame.git"
 
   bottle do
@@ -13,17 +13,11 @@ class RomTools < Formula
     sha256 "c6dc8b0e8bf4c028ba85f62bf4d2d9140cea3d73ee46cc2547a9cbcffc3677c3" => :mountain_lion
   end
 
+  depends_on :python => :build if MacOS.version <= :snow_leopard
   depends_on "sdl2"
   depends_on "flac"
   depends_on "sqlite"
   depends_on "portmidi"
-
-  # FIXME: prevent build failure, fixed upstream
-  # https://github.com/mamedev/mame/pull/292
-  patch do
-    url "https://github.com/mamedev/mame/commit/2afb39374410d803dd287f83de44fbf7a4c2edc8.diff"
-    sha256 "0419953fc46170e2e114fbd369cfa461894b30a8c54c4729917b31575ed078a8"
-  end
 
   def install
     inreplace "scripts/src/main.lua", /(targetsuffix) "\w+"/, '\1 ""'
@@ -34,8 +28,8 @@ class RomTools < Formula
                    "USE_SYSTEM_LIB_FLAC=1",
                    "USE_SYSTEM_LIB_PORTMIDI=1"
     bin.install %W[
-      castool chdman floptool imgtool jedutil ldplayer ldresample ldverify nltool nlwav pngcmp
-      regrep romcmp src2html srcclean testkeys unidasm
+      aueffectutil castool chdman floptool imgtool jedutil ldplayer ldresample
+      ldverify nltool nlwav pngcmp regrep romcmp src2html srcclean testkeys unidasm
     ]
     bin.install "split" => "rom-split"
     man1.install Dir["src/osd/sdl/man/*.1"]
@@ -43,6 +37,7 @@ class RomTools < Formula
 
   # Needs more comprehensive tests
   test do
+    # system "#{bin}/aueffectutil" # segmentation fault
     system "#{bin}/castool"
     # system "#{bin}/chdman"
     system "#{bin}/floptool"
