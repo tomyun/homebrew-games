010a66d61478c8c5fda857f6f707713d44e885f9
Kyungdahm Yun <tomyun@gmail.com>
2015-10-16 11:46:40 -0700

    fmsx 4.3
    Closes #427.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- fmsx.rb
+++ fmsx.rb
@@ -1,9 +1,9 @@
 class Fmsx < Formula
   desc "MSX emulator"
   homepage "http://fms.komkon.org/fMSX/"
-  url "http://fms.komkon.org/fMSX/fMSX42.zip"
-  version "4.2"
-  sha256 "7040bd61b309d4b514c20c7413d48f9b816e2f91e8bf7bb268d6143cbaa75e00"
+  url "http://fms.komkon.org/fMSX/fMSX43.zip"
+  version "4.3"
+  sha256 "b8feba2b052a0426a418c3051c24ae64376eae95c7c5363021484d23df7e30af"
 
   bottle do
     cellar :any
@@ -19,13 +19,13 @@ class Fmsx < Formula
     chdir "fMSX/Unix" do
       inreplace "Makefile" do |s|
         pa = Formula["pulseaudio"]
-        s.gsub! %r[(DEFINES\s*\+=\s*[-/$()\w\t ]*)], "\\1 -DPULSE_AUDIO"
-        s.gsub! %r[(CFLAGS\s*\+=\s*[-/$()\w\t ]*)], "\\1 -I#{pa.include}\nLIBS += -L#{pa.lib} -lpulse-simple"
+        s.gsub! /(DEFINES\s*\+=\s*[-\/$()\w\t ]*)/, "\\1 -DPULSE_AUDIO"
+        s.gsub! /(CFLAGS\s*\+=\s*[-\/$()\w\t ]*)/, "\\1 -I#{pa.include}\nLIBS += -L#{pa.lib} -lpulse-simple"
       end
       system "make"
       bin.install "fmsx"
     end
-    pkgshare.install "ROMs"
+    pkgshare.install "fMSX/ROMs"
   end
 
   def caveats; <<-EOS.undent
