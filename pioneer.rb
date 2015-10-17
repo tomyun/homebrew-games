c129f7cae863f05d91f78643cd4d99a9dc8ddf2e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-17 18:52:39 +0100

    pioneer: update 20151015 bottle.

--- pioneer.rb
+++ pioneer.rb
@@ -6,9 +6,9 @@ class Pioneer < Formula
   head "https://github.com/pioneerspacesim/pioneer.git"
 
   bottle do
-    sha256 "4326eb6c30c63a81c6e973ae81444056521983cd9fe2db49d2a5cc6d3a68c004" => :el_capitan
-    sha256 "24964a0cb3462628c06d046f59d3d9e795cdad8484ec3b566b5dd90f610dbef7" => :yosemite
-    sha256 "fbce5c907ea9df41570c004e38be38776d25a5e50a16bc2d521b4256035f3103" => :mavericks
+    sha256 "8f356d57a3bba80e8a1d71c0341b17ca659e47ea4f952ce939a0b998989f96e4" => :el_capitan
+    sha256 "642b00ccfd4fde699aaf2e03ea15dee7d43e1de53f27a8e9f61577635deae892" => :yosemite
+    sha256 "665d6612aecf9a17c852994feded20a5babb5130f5e209e2a3d59d08158958e9" => :mavericks
   end
 
   depends_on "autoconf" => :build
