b78f2c9f28129a0e0146f09f7411ab66045d0b5e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-04 13:49:37 +0000

    myman: add 2009-1 bottle.

--- myman.rb
+++ myman.rb
@@ -4,6 +4,12 @@ class Myman < Formula
   url "https://downloads.sourceforge.net/project/myman/myman-cvs/myman-cvs-2009-10-30/myman-wip-2009-10-30.tar.gz"
   sha256 "bf69607eabe4c373862c81bf56756f2a96eecb8eaa8c911bb2abda78b40c6d73"
 
+  bottle do
+    sha256 "af34480e0b48213a90fbf0fcae7bc387614f90b1950b54dc894281eed9413508" => :el_capitan
+    sha256 "e47205a17d13c97382bdf1ff5f1d64e45060e63592633c21fb42393854965628" => :yosemite
+    sha256 "53ce0852a35f8d7de924dc952e85903a2c17a0dcaecadda9344fb3262fb33a09" => :mavericks
+  end
+
   depends_on "coreutils" => :build
   depends_on "gnu-sed" => :build
   depends_on "homebrew/dupes/groff" => :build
