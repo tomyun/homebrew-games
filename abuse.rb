7be2baf529be9412ebf83482687c1a51635fa2d7
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-05 15:42:52 +0100

    abuse: audit fixes

--- abuse.rb
+++ abuse.rb
@@ -1,10 +1,9 @@
-require 'formula'
-
 class Abuse < Formula
-  homepage 'http://abuse.zoy.org/'
-  url 'http://abuse.zoy.org/raw-attachment/wiki/download/abuse-0.8.tar.gz'
-  head 'svn://svn.zoy.org/abuse/abuse/trunk'
-  sha1 '12fea968a1494338ad71c6ec24954f3eef56826a'
+  desc "dark 2D side-scrolling platform game"
+  homepage "http://abuse.zoy.org/"
+  url "http://abuse.zoy.org/raw-attachment/wiki/download/abuse-0.8.tar.gz"
+  sha256 "0104db5fd2695c9518583783f7aaa7e5c0355e27c5a803840a05aef97f9d3488"
+  head "svn://svn.zoy.org/abuse/abuse/trunk"
 
   bottle do
     cellar :any
@@ -13,23 +12,23 @@ class Abuse < Formula
     sha1 "d35d4a43e373ca8af58dfb361637498e179188ca" => :mountain_lion
   end
 
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
-  depends_on 'pkg-config' => :build
-  depends_on 'sdl'
-  depends_on 'sdl_mixer'
-  depends_on 'libvorbis'
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "sdl"
+  depends_on "sdl_mixer"
+  depends_on "libvorbis"
 
   def startup_script; <<-EOS.undent
-    #!/bin/bash
-    #{libexec}/abuse-bin -datadir "#{share}/abuse" "$@"
+      #!/bin/bash
+      #{libexec}/abuse-bin -datadir "#{share}/abuse" "$@"
     EOS
   end
 
   def install
     # Hack to work with newer versions of automake
-    inreplace "bootstrap", '11 10 9 8 7 6 5', '$(seq -s " " 5 99)'
+    inreplace "bootstrap", "11 10 9 8 7 6 5", '$(seq -s " " 5 99)'
 
     # Add SDL.m4 to aclocal includes
     inreplace "bootstrap",
@@ -37,14 +36,14 @@ class Abuse < Formula
       "aclocal${amvers} ${aclocalflags} -I#{HOMEBREW_PREFIX}/share/aclocal"
 
     # undefined
-    inreplace 'src/net/fileman.cpp', 'ushort', 'unsigned short'
-    inreplace 'src/sdlport/setup.cpp', 'UInt8', 'uint8_t'
+    inreplace "src/net/fileman.cpp", "ushort", "unsigned short"
+    inreplace "src/sdlport/setup.cpp", "UInt8", "uint8_t"
 
     # Fix autotools obsoletion notice
-    inreplace 'configure.ac', 'AM_CONFIG_HEADER', 'AC_CONFIG_HEADERS'
+    inreplace "configure.ac", "AM_CONFIG_HEADER", "AC_CONFIG_HEADERS"
 
     # Re-enable OpenGL detection
-    inreplace 'configure.ac',
+    inreplace "configure.ac",
       "#error\t/* Error so the compile fails on OSX */",
       '#include <OpenGL/gl.h>'
 
@@ -54,28 +53,28 @@ class Abuse < Formula
                           "--prefix=#{prefix}",
                           "--disable-sdltest",
                           "--with-assetdir=#{share}/abuse",
-                          "--with-sdl-prefix=#{Formula['sdl'].opt_prefix}"
+                          "--with-sdl-prefix=#{Formula["sdl"].opt_prefix}"
 
     # Use Framework OpenGL, not libGl
-    %w[ . src src/imlib src/lisp src/net src/sdlport ].each do |p|
-      inreplace "#{p}/Makefile", '-lGL', '-framework OpenGL'
+    %w[. src src/imlib src/lisp src/net src/sdlport].each do |p|
+      inreplace "#{p}/Makefile", "-lGL", "-framework OpenGL"
     end
 
     system "make"
 
-    bin.install 'src/abuse-tool'
-    libexec.install 'src/abuse' => 'abuse-bin'
-    (share+'abuse').install Dir["data/*"] - %w(data/Makefile data/Makefile.am data/Makefile.in)
+    bin.install "src/abuse-tool"
+    libexec.install "src/abuse" => "abuse-bin"
+    (share/"abuse").install Dir["data/*"] - %w[data/Makefile data/Makefile.am data/Makefile.in]
     # Use a startup script to find the game data
-    (bin+'abuse').write startup_script
+    (bin/"abuse").write startup_script
   end
 
   def caveats; <<-EOS.undent
-    Game settings and saves will be written to the ~/.abuse folder.
+      Game settings and saves will be written to the ~/.abuse folder.
     EOS
   end
 
   test do
-    system("#{libexec}/abuse-bin", '--help')
+    system "#{bin}/abuse", "--help"
   end
 end
