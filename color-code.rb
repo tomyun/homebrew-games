c6973822b912e67d4b55862f2a8d3f21f8e3ede0
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-05 15:33:40 +0100

    color-code: audit fixes
    Closes #438.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- color-code.rb
+++ color-code.rb
@@ -1,14 +1,19 @@
-require 'formula'
-
 class ColorCode < Formula
-  url 'http://colorcode.laebisch.com/download/ColorCode-0.7.2.tar.gz'
-  homepage 'http://colorcode.laebisch.com/'
-  sha1 'd9ecd49d04c41ac680ae4d2cb217215440c1a27d'
+  desc "Free advanced MasterMind clone"
+  homepage "http://colorcode.laebisch.com/"
+  url "http://colorcode.laebisch.com/download/ColorCode-0.7.2.tar.gz"
+  sha256 "d1c5bf4d65c81de16c4159c2c69c096fc7ff47cca587d7233985e078d63c79aa"
 
-  depends_on 'qt'
+  depends_on "qt"
 
   def install
-    system "qmake; make"
+    system "qmake"
+    system "make"
     prefix.install "ColorCode.app"
+    bin.write_exec_script "#{prefix}/ColorCode.app/Contents/MacOS/colorcode"
+  end
+
+  test do
+    system "#{bin}/colorcode", "-h"
   end
 end
