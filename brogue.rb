8c82ad832a3883c1156b2c64bac174524c444cd6
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-06 11:47:55 +0100

    brogue: audit fixes

--- brogue.rb
+++ brogue.rb
@@ -1,35 +1,31 @@
-require 'formula'
-
 class Brogue < Formula
-  homepage 'https://sites.google.com/site/broguegame/'
-
+  desc "Roguelike game"
+  homepage "https://sites.google.com/site/broguegame/"
   # The OS X version doesn't contain a Makefile, so we
   # need to download the Linux version
-  url 'https://sites.google.com/site/broguegame/brogue-1.7.4-linux-amd64.tbz2'
-  sha1 'd2a7f74b040fe29dee81cb1ff8e78f8ee807c1c0'
+  url "https://sites.google.com/site/broguegame/brogue-1.7.4-linux-amd64.tbz2"
   version "1.7.4"
+  sha256 "eba5f35fe317efad9c97876f117eaf7a26956c435fdd2bc1a5989f0a4f70cfd3"
 
   # put the highscores file in HOMEBREW_PREFIX/var/brogue/ instead of a
   # version-dependent location.
   patch :DATA
 
   def install
-    mkdir_p "#{var}/brogue"
+    (var/"brogue").mkpath
 
-    doc.install 'Readme.rtf' => 'README.rtf'
-    doc.install 'agpl.txt' => 'COPYING'
+    doc.install "Readme.rtf" => "README.rtf"
+    doc.install "agpl.txt" => "COPYING"
 
-    system "make", "clean"
-    system "make", "curses"
+    system "make", "clean", "curses"
 
     # The files are installed in libexec
     # and the provided `brogue` shell script,
     # which is just a convenient way to launch the game,
     # is placed in the `bin` directory.
-    inreplace 'brogue', /`dirname \$0`\/bin$/, libexec
-    bin.install 'brogue'
-    libexec.install 'bin/brogue'
-    libexec.install 'bin/keymap'
+    inreplace "brogue", /`dirname \$0`\/bin$/, libexec
+    bin.install "brogue"
+    libexec.install "bin/brogue", "bin/keymap"
   end
 
   def caveats; <<-EOS.undent
