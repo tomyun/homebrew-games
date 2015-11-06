52d5027711886cf3b3a44e891b460efcf22cc4f2
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-06 11:52:35 +0100

    c10t: test added
    Closes #441.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- c10t.rb
+++ c10t.rb
@@ -1,33 +1,32 @@
-require "formula"
-
 class C10t < Formula
+  desc "Minecraft cartography tool"
   homepage "https://github.com/udoprog/c10t"
   url "https://github.com/udoprog/c10t/archive/1.7.tar.gz"
-  sha1 "1419b0abd42b05c82cde39d02b4ffce1e77265d1"
+  sha256 "0e5779d517105bfdd14944c849a395e1a8670bedba5bdab281a0165c3eb077dc"
 
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "freetype"
 
   # Needed to compile against newer boost
-  # Can be removed for next version of c10t after 1.7
+  # Can be removed for the next version of c10t after 1.7
   # See: https://github.com/udoprog/c10t/pull/153
   patch do
     url "https://github.com/udoprog/c10t/commit/4a392b9f06d08c70290f4c7591e84ecdbc73d902.diff"
-    sha1 "e528aef909138a596ae0c7a48e84593b816a27f0"
+    sha256 "f2d7b1772672f7769adab0eaf081887399de772e72ad1fa53caa0856d50b9a8a"
   end
 
   # Fix freetype detection; adapted from this upstream commit:
   # https://github.com/udoprog/c10t/commit/2a2b8e49d7ed4e51421cc71463c1c2404adc6ab1
   patch do
     url "https://gist.githubusercontent.com/mistydemeo/f7ab02089c43dd557ef4/raw/a0ae7974e635b8ebfd02e314cfca9aa8dc95029d/c10t-freetype.diff"
-    sha1 "088ba840b8f636aae62fd70cc32b963fedadf233"
+    sha256 "9fbb7ccc643589ac1d648e105369e63c9220c26d22f7078a1f40b27080d05db4"
   end
 
   # Ensure zlib header is included for libpng; fixed upstream
   patch do
     url "https://github.com/udoprog/c10t/commit/800977bb23e6b4f9da3ac850ac15dd216ece0cda.diff"
-    sha1 "706fc266f7ba064c3414f3ab6fef90301386cff0"
+    sha256 "4c8953bdc46b1b2063abe0d1768e7116b9092d49f16161732e39e4abb5622ec1"
   end
 
   def install
@@ -35,5 +34,9 @@ class C10t < Formula
     system "cmake", ".", *std_cmake_args
     system "make"
     bin.install "c10t"
+  end
+
+  test do
+    system "#{bin}/c10t", "--list-colors"
   end
 end
