121de1ed8606deb9b527109d3816071287b0267f
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-06 11:21:25 +0100

    avanor: test added
    Closes #440.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- avanor.rb
+++ avanor.rb
@@ -1,19 +1,31 @@
-require 'formula'
-
 class Avanor < Formula
-  url 'https://downloads.sourceforge.net/project/avanor/avanor/0.5.8/avanor-0.5.8-src.tar.bz2'
-  homepage 'http://avanor.sourceforge.net/'
-  sha1 '5685ca96a203a7f7ada733dc34b18faab440d189'
+  desc "Quick-growing roguelike game with easy ADOM-like UI"
+  homepage "http://avanor.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/avanor/avanor/0.5.8/avanor-0.5.8-src.tar.bz2"
+  sha256 "8f55be83d985470b9a5220263fc87d0a0a6e2b60dbbc977c1c49347321379ef3"
 
   # Upstream fix for clang: http://sourceforge.net/p/avanor/code/133/
   patch :p0 do
     url "https://gist.githubusercontent.com/mistydemeo/64f47233ee64d55cb7d5/raw/c1847d7e3a134e6109ad30ce1968919dd962e727/avanor-clang.diff"
-    sha1 "f2b83c1ff5f2ad45ee19b663232f65b0d1238372"
+    sha256 "2d24ce7b71eb7b20485d841aabffa55b25b9074f9a5dd83aee33b7695ba9d75c"
   end
 
   def install
     system "make", "DATA_DIR=#{share}/avanor/", "CC=#{ENV.cxx}", "LD=#{ENV.cxx}"
     bin.install "avanor"
-    (share+"avanor").install "manual"
+    (share/"avanor").install "manual"
+  end
+
+  test do
+    script = (testpath/"script.exp")
+    script.write <<-EOS.undent
+      #!/usr/bin/expect -f
+      set timeout 10
+      spawn avanor
+      send -- "\e"
+      expect eof
+    EOS
+    script.chmod 0700
+    system "./script.exp"
   end
 end
