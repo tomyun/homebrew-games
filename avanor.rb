e9061a876f416791e220f6e516a05b7a8ab50e39
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-06 10:24:22 +0000

    avanor: add 0.5.8 bottle.

--- avanor.rb
+++ avanor.rb
@@ -4,6 +4,12 @@ class Avanor < Formula
   url "https://downloads.sourceforge.net/project/avanor/avanor/0.5.8/avanor-0.5.8-src.tar.bz2"
   sha256 "8f55be83d985470b9a5220263fc87d0a0a6e2b60dbbc977c1c49347321379ef3"
 
+  bottle do
+    sha256 "89c8215f9e670819e065820a3fd59dee56c7e59f9e304f799f2b319db372f998" => :el_capitan
+    sha256 "0c8cce6920444c2b7d6ea524bc9881f2e19c6ced735bb4f833cb6e04c49c5ffc" => :yosemite
+    sha256 "0899dfe6dbc824bdf5b589020063c711c51fde9a6cca87048ec352779c2cbcf1" => :mavericks
+  end
+
   # Upstream fix for clang: http://sourceforge.net/p/avanor/code/133/
   patch :p0 do
     url "https://gist.githubusercontent.com/mistydemeo/64f47233ee64d55cb7d5/raw/c1847d7e3a134e6109ad30ce1968919dd962e727/avanor-clang.diff"
