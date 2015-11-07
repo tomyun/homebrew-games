5fbda5402cf27128722c2d5178448925fa5a5430
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-07 14:03:03 +0100

    mupen64plus-video-glide64mk2: audit fixes

--- mupen64plus-video-glide64mk2.rb
+++ mupen64plus-video-glide64mk2.rb
@@ -1,17 +1,20 @@
 class Mupen64plusVideoGlide64mk2 < Formula
-  homepage 'https://code.google.com/p/mupen64plus/'
-  url 'https://bitbucket.org/richard42/mupen64plus-video-glide64mk2/downloads/mupen64plus-video-glide64mk2-src-2.0.tar.gz'
-  sha1 '4ee837d711fb5698d9cdbb7eb0204e8fb86d110e'
+  desc "Video plugin for Mupen64Plus 2.0"
+  homepage "http://www.mupen64plus.org/"
+  url "https://github.com/mupen64plus/mupen64plus-video-glide64mk2/releases/download/2.5/mupen64plus-video-glide64mk2-src-2.5.tar.gz"
+  sha256 "ef3dae0084e078d843605abdf5039eb8b5dd68ff1410b4fc12bdf19592a9fcb6"
 
-  depends_on 'pkg-config' => :build
+  depends_on "pkg-config" => :build
   depends_on "libpng"
-  depends_on 'sdl'
-  depends_on 'boost'
-  depends_on 'mupen64plus'
+  depends_on "sdl"
+  depends_on "boost"
+  depends_on "mupen64plus"
 
   def install
-    cd "projects/unix" do
-      system "make", "install", "PREFIX=#{prefix}", "C_INCLUDE_PATH=/usr/local/include", "CPLUS_INCLUDE_PATH=/usr/local/include", "V=1"
-    end
+    system "make", "install", "-C", "projects/unix",
+                              "PREFIX=#{prefix}",
+                              "C_INCLUDE_PATH=/usr/local/include",
+                              "CPLUS_INCLUDE_PATH=/usr/local/include",
+                              "V=1"
   end
 end
