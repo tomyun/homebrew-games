212f79ec9851f1aa4099e61c9db4b8915a456646
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-07 01:48:33 +0100

    darf-fortress: audit fixes

--- dwarf-fortress.rb
+++ dwarf-fortress.rb
@@ -1,13 +1,14 @@
 class DwarfFortress < Formula
+  desc "Open-ended rogelike game"
   homepage "http://bay12games.com/dwarves/"
   url "http://bay12games.com/dwarves/df_40_24_osx.tar.bz2"
   version "0.40.24"
-  sha1 "e9e5c5d9d5c2dd4c8078060b705829d45231051d"
+  sha256 "653837ed073ffe2825d25bc80f0967d6a8ccb9bf869a188d4bd4320154630936"
 
   depends_on :x11
 
   def install
-    (bin+"dwarffortress").write <<-EOS.undent
+    (bin/"dwarffortress").write <<-EOS.undent
       #!/bin/sh
       exec #{libexec}/df
     EOS
@@ -16,8 +17,8 @@ class DwarfFortress < Formula
   end
 
   def caveats; <<-EOS.undent
-    If you're using a retina display, the PRINT_MODE should
-    be changed to STANDARD in #{libexec}/data/init/init.txt
+      If you're using a retina display, the PRINT_MODE should
+      be changed to STANDARD in #{libexec}/data/init/init.txt
     EOS
   end
 end
