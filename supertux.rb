fe7f272a21d166aa15e3254ad549285369310a76
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-13 13:06:16 +0000

    supertux: add 0.1.3 bottle.

--- supertux.rb
+++ supertux.rb
@@ -10,6 +10,12 @@ class Supertux < Formula
     depends_on "sdl_image"
     depends_on "sdl_mixer" => "with-libvorbis"
   end
+  bottle do
+    sha256 "5dce574690fa4f983731166849a339a6afbce201bd4abef609ea986297e2b495" => :el_capitan
+    sha256 "bb4a521d8d992934a68fe6227091f42e100546b20499ec12aba65edf6d2cd962" => :yosemite
+    sha256 "f3fde4a559bd5d3edbd9af221f89322f2d3a83c67ef998dcabc0aea42ab9fe83" => :mavericks
+  end
+
 
   devel do
     url "https://github.com/SuperTux/supertux/releases/download/v0.3.5a/supertux-0.3.5a.tar.bz2"
