6af95ba330b840a30508fa0c996e32d6d31ffa72
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-20 12:36:17 +0000

    naga: update 1.0 bottle.

--- naga.rb
+++ naga.rb
@@ -5,10 +5,11 @@ class Naga < Formula
   sha256 "7f56b03b34e2756b9688e120831ef4f5932cd89b477ad8b70b5bcc7c32f2f3b3"
 
   bottle do
-    cellar :any
-    sha1 "6ca134f2efac554b07e770139c3c33f4df6feae6" => :yosemite
-    sha1 "530bf68d68d32def280055ca3c07884b24c9538d" => :mavericks
-    sha1 "89f602eae5136fb53ec8e816ae384ac8a181b1a1" => :mountain_lion
+    cellar :any_skip_relocation
+    revision 1
+    sha256 "d463ce4c2bab8ca72fcdee4545592a5de2d4eb4c30ae83d8bc7e59aff65b9b37" => :el_capitan
+    sha256 "c1e7b2d084acd6f9c902dd012f56c3f4bb0e8e6482cea24d6416dcd9ec5fe540" => :yosemite
+    sha256 "79beeb5cf00056d30ca4ca8173b6c992d562ab4b948e62d636fbc599b17a9bba" => :mavericks
   end
 
   def install
