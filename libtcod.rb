f6247bfbf7d166a88602a106a3f75b3ae54b536a
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-25 15:01:49 +0100

    libtcod: desc added

--- libtcod.rb
+++ libtcod.rb
@@ -1,10 +1,11 @@
 class Libtcod < Formula
-  homepage 'http://roguecentral.org/doryen/libtcod/'
-  url 'https://bitbucket.org/libtcod/libtcod/get/1.5.1.tar.bz2'
-  sha256 '290145f760371881bcc6aa3fda256a2927f9210acc3f0a7230c5dfd57d9052d0'
+  desc "API for roguelike developpers"
+  homepage "http://roguecentral.org/doryen/libtcod/"
+  url "https://bitbucket.org/libtcod/libtcod/get/1.5.1.tar.bz2"
+  sha256 "290145f760371881bcc6aa3fda256a2927f9210acc3f0a7230c5dfd57d9052d0"
 
-  depends_on 'cmake' => :build
-  depends_on 'sdl'
+  depends_on "cmake" => :build
+  depends_on "sdl"
 
   def install
     # Remove unnecessary X11 check - our SDL doesn't use X11
@@ -17,10 +18,10 @@ class Libtcod < Formula
     system "make"
 
     # cmake produces an install target, but it installs nothing
-    lib.install 'src/libtcod.dylib'
-    lib.install 'src/libtcod-gui.dylib'
-    include.install Dir['include/*']
+    lib.install "src/libtcod.dylib"
+    lib.install "src/libtcod-gui.dylib"
+    include.install Dir["include/*"]
     # don't yet know what this is for
-    libexec.install 'data'
+    libexec.install "data"
   end
 end
