994a723a9a344b98ab976a964978043188bfa854
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-25 12:51:12 +0100

    tty-solitaire: desc added

--- tty-solitaire.rb
+++ tty-solitaire.rb
@@ -1,4 +1,5 @@
 class TtySolitaire < Formula
+  desc "Ncurses-based klondike solitaire game"
   homepage "https://github.com/mpereira/tty-solitaire"
   url "https://github.com/mpereira/tty-solitaire/archive/v0.2.0.tar.gz"
   sha256 "9d47c5a88b3d70d19acfdff29532896b7905e9ddc20b29730e3a8cb92ff3cca4"
