9ca1c35e84791a5d0abe8d2ad748b9f56b76ae06
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-25 14:55:56 +0100

    homeworlds: desc added

--- homeworlds.rb
+++ homeworlds.rb
@@ -1,4 +1,5 @@
 class Homeworlds < Formula
+  desc "C++ framework for the game of Binary Homeworlds"
   homepage "https://github.com/Quuxplusone/Homeworlds/"
   url "https://github.com/Quuxplusone/Homeworlds.git",
     :revision => "917cd7e7e6d0a5cdfcc56cd69b41e3e80b671cde"
