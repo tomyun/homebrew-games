9581265e14000e855a63d983f9ba2f469d8aca4a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-27 06:40:00 +0000

    yetris: add 2.3.0 bottle.

--- yetris.rb
+++ yetris.rb
@@ -4,6 +4,13 @@ class Yetris < Formula
   url "https://github.com/alexdantas/yetris/archive/v2.3.0.tar.gz"
   sha256 "720c222325361e855e2dcfec34f8f0ae61dd418867a87f7af03c9a59d723b919"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6b6a16e41ac6321347a7609cda1fbc56bd10dab8ae84182e606e09c44cf59ed9" => :el_capitan
+    sha256 "07f963700d2445ff2a2b66c4aaafa236604050f515b46ac2661ffda0ce2913ec" => :yosemite
+    sha256 "c77601064ff9c03eda3ca9ef8a97043b2abc130f5dbef707f7ab1001ed6c0768" => :mavericks
+  end
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
