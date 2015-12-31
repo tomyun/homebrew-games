5ddf2eb392bb1a38fefc9d647a5b986203521e48
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-31 05:03:42 +0000

    rlvm: add 0.14 bottle.

--- rlvm.rb
+++ rlvm.rb
@@ -5,6 +5,13 @@ class Rlvm < Formula
   sha256 "6d1717540b8db8aca1480ebafae3354b24e3122a77dd2ee81f4b964c7b10dcc0"
   head "https://github.com/eglaysher/rlvm.git"
 
+  bottle do
+    cellar :any
+    sha256 "42db404c1cb69675c5df92cc389df79c989816c1ea44897361383b918d12a708" => :el_capitan
+    sha256 "8230da353062d3b7d4189297b60f9f5e9b09e0c16af55ed65d9cbb4c49c342ff" => :yosemite
+    sha256 "edd96b68a808c86d76df7f3ec60dd7661e8fdb37bbb5b148c7e782887f2120fa" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "scons" => :build
   depends_on "boost"
