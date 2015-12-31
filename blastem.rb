606aaeff2f1e4d3073289203fc3e6c430ee2614d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-31 01:24:05 +0000

    blastem: add 0.3.1 bottle.

--- blastem.rb
+++ blastem.rb
@@ -5,6 +5,13 @@ class Blastem < Formula
   version "0.3.1"
   head "http://rhope.retrodev.com/repos/blastem"
 
+  bottle do
+    cellar :any
+    sha256 "91409efedd7f6736faefe4bff23a99e2b49af3ae52b24311113cd76f7622e66d" => :el_capitan
+    sha256 "f4e396fe0fe13554903d2186b7c81dd7bc9c372519adfc1196619be34e7f01a2" => :yosemite
+    sha256 "d2786d311d8538275ea7155b818455c95601e316dc6c1fe335e655afa3b84755" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "glew"
   depends_on "sdl2"
