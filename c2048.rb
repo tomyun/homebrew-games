1221c81fa0db47feb92ce8045cc7a4fa3d7bf60b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-02 03:43:57 +0000

    c2048: add 0+20150805 bottle.

--- c2048.rb
+++ c2048.rb
@@ -5,6 +5,13 @@ class C2048 < Formula
   version "0+20150805"
   head "https://github.com/mevdschee/2048.c.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "150f562e3a85e962e576fe172353fc709483cb9b49f47dd05b0248fb0b238f0e" => :el_capitan
+    sha256 "aedb7caed6f1bfeb343a12df5cb62b060a92cbfb27f22af6d4aede12968d92c2" => :yosemite
+    sha256 "bfee60cbfe49b6f5b4a4dc14f6f83a7e521f7c5fe651dd701307dd185de798e5" => :mavericks
+  end
+
   def install
     system "make"
     bin.install "2048"
