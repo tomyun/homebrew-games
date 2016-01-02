f405c2ecb3fdc53857ced44a13b194908d30ac9d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-02 02:47:29 +0000

    hatari: add 1.9.0 bottle.

--- hatari.rb
+++ hatari.rb
@@ -5,6 +5,13 @@ class Hatari < Formula
   sha256 "b2b56e6cbe7f8769a5e8b1d96599f392d9351b44cacf959da6905da06d30e992"
   head "http://hg.tuxfamily.org/mercurialroot/hatari/hatari", :using => :hg, :branch => "default"
 
+  bottle do
+    cellar :any
+    sha256 "e0011e9e4f0c70954d10082c0665e46b49fd98dd41e61928247f710c9f5d9129" => :el_capitan
+    sha256 "0353909d1e922d9b0ae77b4b89b99297769544d0565cfd40f2b01a3065bcf9a6" => :yosemite
+    sha256 "a652a30950ea90b36d8c550b54a30413fafc6918a8385bddaecb5d1de3ceada4" => :mavericks
+  end
+
   depends_on "cmake" => :build
   depends_on "libpng"
   depends_on "sdl"
