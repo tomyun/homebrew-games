2a325a103ac57cbf31e67a650af7e98122c93073
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-02 03:30:39 +0000

    angolmois: add 2.0.0alpha2 bottle.

--- angolmois.rb
+++ angolmois.rb
@@ -11,6 +11,13 @@ class Angolmois < Formula
     depends_on "sdl_mixer" => "with-smpeg"
     depends_on "smpeg"
   end
+  bottle do
+    cellar :any
+    sha256 "da08947ee8e73da3a850420332b6c0961e0aee1b9d1a5f44e7653ded789a8bb5" => :el_capitan
+    sha256 "b23d0e9091916d0a82be335af3eea1cf46b63de95b0c844d486ff5ff5ae45247" => :yosemite
+    sha256 "e48b08dafd1f8022fd72e1a1e634fb5c47c1ef3430689869c1dffa7585c6148a" => :mavericks
+  end
+
 
   head do
     url "https://github.com/lifthrasiir/angolmois.git"
