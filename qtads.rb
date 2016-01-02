b169c98ada953d8102e57fe71a867fc6d22f25e1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-02 03:18:34 +0000

    qtads: add 2.1.6 bottle.

--- qtads.rb
+++ qtads.rb
@@ -5,6 +5,13 @@ class Qtads < Formula
   sha256 "33f29b4c1b225f1a1e7ad1405a1bb7eb8ec4b1d567eac48a120071f7793373c8"
   head "https://github.com/realnc/qtads.git"
 
+  bottle do
+    cellar :any
+    sha256 "3247cdf051f31f837b86571cf0dc0818bc7a8399e00a81431c1e1da8bafe346c" => :el_capitan
+    sha256 "200bb09ea75e8bfa4dd266544c266eddd7bcc1e03c3ac3be0f4a57aa09cb2330" => :yosemite
+    sha256 "d320c148ada10a316b5aca7b659b8e0875d2ae50e862a66c80a2eb10a3f091f0" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "qt"
   depends_on "sdl"
