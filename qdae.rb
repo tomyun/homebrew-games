eeff4632bf06f339a57d4ec1163c6bdca27b7205
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-02 02:52:00 +0000

    qdae: add 0.0.10 bottle.

--- qdae.rb
+++ qdae.rb
@@ -4,6 +4,12 @@ class Qdae < Formula
   url "http://www.seasip.info/Unix/QDAE/qdae-0.0.10.tar.gz"
   sha256 "780752c37c9ec68dd0cd08bd6fe288a1028277e10f74ef405ca200770edb5227"
 
+  bottle do
+    sha256 "8f8bacc65658df8d20648da83bfb6cb73738ce2f50c4c8dd96b3d59ee2238046" => :el_capitan
+    sha256 "7c6215e807e24d1a22e9186329c17fb7cdcf19cba797483e6340aecfe42e3c36" => :yosemite
+    sha256 "a0c2bc5437cdb373ec43932e98237daa3a33b330afa0c68122c9de6afd94836d" => :mavericks
+  end
+
   depends_on "sdl"
   depends_on "libxml2"
 
