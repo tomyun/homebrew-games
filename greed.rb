ea56839b5100b72ebd50016af4dd4b160968cf53
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-02 03:47:38 +0000

    greed: add 4.1 bottle.

--- greed.rb
+++ greed.rb
@@ -5,6 +5,13 @@ class Greed < Formula
   sha256 "2356151b2f95badcb8ad413ca88ee7022a46b11b2edab5b096de6d033778b1ea"
   head "https://gitlab.com/esr/greed.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9a34bc7c0d767d0687f2d302173b1a93977512f11106e78269e82faf8d824957" => :el_capitan
+    sha256 "122677213cbe13f011da1b06ebea4f24812c6c8a3c68f702151922f665dfaaec" => :yosemite
+    sha256 "72c17694248c5bd5a8cba9947e2f04a6475116b8ed5d04a6b45a35c6f95101fb" => :mavericks
+  end
+
   def install
     # Handle hard-coded destination
     inreplace "Makefile", "/usr/share/man/man6", man6
