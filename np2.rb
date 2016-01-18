4fd9d7a31d9270df06f65f33ddae42e890b84177
Kyungdahm Yun <tomyun@gmail.com>
2016-01-18 11:17:25 -0800

    np2 0.85
    Closes #490.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- np2.rb
+++ np2.rb
@@ -1,16 +1,15 @@
 class Np2 < Formula
   desc "Neko Project 2: PC-9801 emulator"
   homepage "http://www.yui.ne.jp/np2/"
-  url "http://amethyst.yui.ne.jp/svn/pc98/np2/tags/VER_0_84/", :using => :svn, :revision => "1735"
-  sha256 "c21130b82dc1cd946c657598726d113891739343100aa63b3a15d9fb9e23eb68"
+  url "http://amethyst.yui.ne.jp/svn/pc98/np2/tags/VER_0_85/", :using => :svn, :revision => "2408"
+  head "http://amethyst.yui.ne.jp/svn/pc98/np2/trunk/", :using => :svn
+
   bottle do
     cellar :any
     sha256 "79a012d83dfbeda5cc093e2556c93d6492acc5aebbf97da7119009ff024e4513" => :yosemite
     sha256 "07dd43fc72d3268d451861f44d05bd4786dda23e3bd81054aa54231afbd93017" => :mavericks
     sha256 "c45a27bbb88896776406f03b6d83192fa8c567f8506358b36e562ab0c0dfc8d9" => :mountain_lion
   end
-
-  head "http://amethyst.yui.ne.jp/svn/pc98/np2/trunk/", :using => :svn
 
   depends_on :xcode => :build
   depends_on "sdl2"
@@ -40,5 +39,9 @@ class Np2 < Formula
   def caveats; <<-EOS.undent
     A Japanese TTF file named `default.ttf` should be in the working directory.
     EOS
+  end
+
+  test do
+    assert_match /Usage: #{bin}\/np2/, shell_output("#{bin}/np2 -h", 1)
   end
 end
