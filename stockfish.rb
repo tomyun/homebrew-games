0490bcf447e0746759bfebe74ee04533eef2afcd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-05 20:29:23 +0000

    stockfish: update 7 bottle.

--- stockfish.rb
+++ stockfish.rb
@@ -6,10 +6,10 @@ class Stockfish < Formula
   head "https://github.com/official-stockfish/Stockfish.git"
 
   bottle do
-    cellar :any
-    sha256 "622bacb36395cb7ccdb83510c354292d954aa655201fdc506185de99b18de75d" => :yosemite
-    sha256 "f02a0eaa017869d57cdb713ab8e20c70122ed17fa60f8add1f2db1933b843ec2" => :mavericks
-    sha256 "6dda7191589eb57f3145681eeb6868dc528d0a3ccd217213177378c57f6a3873" => :mountain_lion
+    cellar :any_skip_relocation
+    sha256 "f05f74604a569a622f50e2750c37c24afd5177d4523797dc25120cbdbb5243fa" => :el_capitan
+    sha256 "b473374b47d14bf9ef7ad173f61851622361e68779257cdde5ef747fe34896ef" => :yosemite
+    sha256 "091dbe159b21e95a4c5f1aaba64390ae4478c694462a203719df6e1ac3f5505e" => :mavericks
   end
 
   def install
