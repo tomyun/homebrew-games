d41f1d5c20f64bef25e29b4f3e454c1ed743eb4a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-13 00:05:04 +0000

    jpcsp: add 0.7+20160203 bottle.

--- jpcsp.rb
+++ jpcsp.rb
@@ -5,6 +5,13 @@ class Jpcsp < Formula
   version "0.7+20160203"
   head "https://github.com/jpcsp/jpcsp.git"
 
+  bottle do
+    cellar :any
+    sha256 "e584e73a5ce65f7e7b16448a7ac77c0fb0a78284f95e4dca913633dc7c56ddfb" => :el_capitan
+    sha256 "a191fcb7a169c762071b6ad98075582d1044911d41adddcdeeb9fcf5e0db6597" => :yosemite
+    sha256 "68ada314543335cd2832d80db2f15a043fda8c22ac7318a3aeb43e452280556f" => :mavericks
+  end
+
   depends_on "ant" => :build
   depends_on "p7zip" => :build
   depends_on :java => "1.6+"
