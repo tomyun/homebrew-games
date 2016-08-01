bfd3af07bbc4fe87bdeb78a99537986e51b74538
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-01 12:22:16 +0100

    xboard: update 4.9.1 bottle.

--- xboard.rb
+++ xboard.rb
@@ -6,9 +6,9 @@ class Xboard < Formula
   sha256 "2b2e53e8428ad9b6e8dc8a55b3a5183381911a4dae2c0072fa96296bbb1970d6"
 
   bottle do
-    sha256 "962538e8ccc89f02b43c29747864591b0bf2a97087addb5ee9695acf262bba6b" => :yosemite
-    sha256 "ddfcc515d630a777c1512df1f29189eb9e0c7f47246d42b1f5ba8b8614fbe81b" => :mavericks
-    sha256 "67f9d1b523f0d387f91e92ee9874ff14e5cf7cb3e8e5473bac7353567e471cde" => :mountain_lion
+    sha256 "07f61bc06c4beb4be13818b5fbde4b91b131610541f18afc7b5bff645f729bea" => :el_capitan
+    sha256 "803662506f9c005806d866f0f68659d0ddce7a57bd0e5b87b4f10e0c17b74edd" => :yosemite
+    sha256 "f35f2cd5fb44fd7190b98c0ef7650473f0c6480ba8b1f55458ca45fef9452ed5" => :mavericks
   end
 
   depends_on "pkg-config" => :build
