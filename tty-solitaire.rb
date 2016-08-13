5056c0937886782b639f59e624c45095b1a85e95
Alex Dunn <dunn.alex@gmail.com>
2016-08-13 12:19:57 -0700

    tty-solitaire 0.2.1
    Closes #624.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- tty-solitaire.rb
+++ tty-solitaire.rb
@@ -1,8 +1,8 @@
 class TtySolitaire < Formula
   desc "Ncurses-based klondike solitaire game"
   homepage "https://github.com/mpereira/tty-solitaire"
-  url "https://github.com/mpereira/tty-solitaire/archive/v0.2.0.tar.gz"
-  sha256 "9d47c5a88b3d70d19acfdff29532896b7905e9ddc20b29730e3a8cb92ff3cca4"
+  url "https://github.com/mpereira/tty-solitaire/archive/v0.2.1.tar.gz"
+  sha256 "ed83b71f16bb477f2870bfca59dd4480f9bd204cccb7155315978f88916abee1"
 
   bottle do
     cellar :any
