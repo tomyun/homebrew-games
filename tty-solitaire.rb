32e26fd6838218a69830003d90fcb67667fa8372
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-13 20:27:12 +0100

    tty-solitaire: update 0.2.1 bottle.

--- tty-solitaire.rb
+++ tty-solitaire.rb
@@ -5,10 +5,10 @@ class TtySolitaire < Formula
   sha256 "ed83b71f16bb477f2870bfca59dd4480f9bd204cccb7155315978f88916abee1"
 
   bottle do
-    cellar :any
-    sha256 "20646ef6b93da06b830c00390a5942a44995c1f64680a8cdeeeeeaff610a6dd1" => :yosemite
-    sha256 "cfad5566ef6677f0e64b870dd6fc686dcbfe0ffe2d0f0dc0bf10aad804e9ef64" => :mavericks
-    sha256 "d827667238329aa0e9071f97114e5f2fba2583c7a5b7c54945af8aa120a25398" => :mountain_lion
+    cellar :any_skip_relocation
+    sha256 "be50a8c8efd4b0d6e400be67e23b655ab39cda9420bef1b89bb2fea4447439a4" => :el_capitan
+    sha256 "edce5a6e4ec649811948a751094cdae28763a765131c06046b9251eca152b549" => :yosemite
+    sha256 "0c7c9114a1bbbc94be747456c2cc60a75f412b47a2852d04f8cac80a993a0e6a" => :mavericks
   end
 
   def install
