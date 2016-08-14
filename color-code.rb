ecab8e4b5231a8244b31844c996c439598f51c5c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-14 19:59:42 +0100

    color-code: update 0.8.5 bottle.

--- color-code.rb
+++ color-code.rb
@@ -6,9 +6,9 @@ class ColorCode < Formula
 
   bottle do
     cellar :any
-    sha256 "4385158349a2eeb7503fc3b7a6a97d3f9235a3686eaf9a0c2f251efa6ff1bec0" => :el_capitan
-    sha256 "c5bf7b180d9a6fbf4e0564647e640eaa62e91969ce24684ce6f650d60dde0074" => :yosemite
-    sha256 "d82d9e6fef4c86b55f13dc681b9fd117a86d4885b79e43960b2cbb28cec168aa" => :mavericks
+    sha256 "92a73a595c94c657676c5b0380c3a6aef9de725e88dde65656e9848e0fe81605" => :el_capitan
+    sha256 "c10fcb700a0c47abef3727c17dd689215e164102a16ac3aad9444f29b7fb9139" => :yosemite
+    sha256 "e06d67b2033015f2112fdee7dcd20de8057aa112be7cd156f87c0e0b36e19ba7" => :mavericks
   end
 
   depends_on "qt5"
