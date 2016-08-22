39f3e410ce83e7e095b969ab4569ffb1ea8ad940
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-22 02:11:24 +0100

    instead: add 2.4.1 bottle.

--- instead.rb
+++ instead.rb
@@ -5,6 +5,12 @@ class Instead < Formula
   sha256 "2368d6e7a38ce1e82feea3848d10916f52caf6103579ac5fbc2bd6dcfdf89c6e"
   head "https://github.com/instead-hub/instead.git"
 
+  bottle do
+    sha256 "7e363add5c92102b75aa71690bfcf68279aa573a06fb6e63457e840ad4111502" => :el_capitan
+    sha256 "9db3e6ff43659ea9b63ad59dbf27ee02d159d137d3e6aeabb2bffbab93b35f3b" => :yosemite
+    sha256 "09210457cf3c12e9afa5fc9e5a14cfe09a0c31ea072f9bee041125d68de79535" => :mavericks
+  end
+
   depends_on "cmake" => :build
   depends_on "lua"
   depends_on "sdl"
