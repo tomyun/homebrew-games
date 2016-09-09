343e694f2e6f448177b4e06d1381c8b7abc0322d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-09 15:12:56 +0100

    pachi: add 11.00 bottle.

--- pachi.rb
+++ pachi.rb
@@ -5,6 +5,13 @@ class Pachi < Formula
   sha256 "2aaf9aba098d816d20950d283c8eaed522f3fa71f68390a4c384c0c1ab03cd6f"
   head "https://github.com/pasky/pachi.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3a9dca7f4c14757f4da0b299118e9dc5d91eb3f47c117bd8ffe386c2b69c44c0" => :el_capitan
+    sha256 "ac6615ed1db69c5daeb24a0ccdaa0bca3abe35d6ae3b91c0d8c952b76b52ec02" => :yosemite
+    sha256 "65ae6afcb334806452449890d82bcf03c670c09345802ca977de6cbd7961ad99" => :mavericks
+  end
+
   fails_with :clang if MacOS.version <= :mavericks
 
   option "without-patterns", "Don't download pattern files for improved performance"
