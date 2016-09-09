9129cceb9914a8d126fce1a8a9e9c949ab5f9a9a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-09 07:14:50 +0100

    libggz: add 0.0.14.1 bottle.

--- libggz.rb
+++ libggz.rb
@@ -4,6 +4,13 @@ class Libggz < Formula
   url "http://mirrors.ibiblio.org/ggzgamingzone/ggz/0.0.14.1/libggz-0.0.14.1.tar.gz"
   sha256 "54301052a327f2ff3f2d684c5b1d7920e8601e13f4f8d5f1d170e5a7c9585e85"
 
+  bottle do
+    cellar :any
+    sha256 "4d7d16c238744f95a8a8036071fc1f91ef2208b739d0b3a3aeba2ee5ed21fb61" => :el_capitan
+    sha256 "70c9a8476e4b7089b62e119ecec56734fcd5d2d44995b9a7cf6fb3f85baf79ab" => :yosemite
+    sha256 "a72920286680f0f8f38ef0dca92908e6da98e8b88fa74351616110b112c85c49" => :mavericks
+  end
+
   # Libggz of this version is unable to build with gnutls-30 and later.
   depends_on "libgcrypt"
   depends_on "gettext"
