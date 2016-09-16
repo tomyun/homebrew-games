a798a286faba24a7ac8eae0b485f1495987dc3a1
Chris Vickio <chris@vickio.net>
2016-09-16 23:13:38 +0300

    glulxe: use glk formulae instead of resources
    Closes #670.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- glulxe.rb
+++ glulxe.rb
@@ -6,28 +6,38 @@ class Glulxe < Formula
   sha256 "2f056987b38cf5bab7c2990cd13e493628dd0774583fb26ff20c338be8343ca6"
   head "https://github.com/erkyrath/glulxe.git"
 
-  # http://www.eblong.com/zarf/glk/index.html
-  resource "glkterm" do
-    url "http://www.eblong.com/zarf/glk/glkterm-104.tar.gz"
-    version "1.0.4"
-    sha256 "473d6ef74defdacade2ef0c3f26644383e8f73b4f1b348e37a9bb669a94d927e"
-  end
+  option "with-glkterm", "Build with glkterm (without wide character support)"
+
+  depends_on "cheapglk" => [:build, :optional]
+  depends_on "glkterm" => [:build, :optional]
+  depends_on "glktermw" => :build if build.without?("cheapglk") && build.without?("glkterm")
 
   def install
-    glkterm_lib = libexec/"glkterm/lib"
-    glkterm_include = libexec/"glkterm/include"
-
-    resource("glkterm").stage do
-      system "make"
-      glkterm_lib.install "libglkterm.a"
-      glkterm_include.install "glk.h", "glkstart.h", "gi_blorb.h", "gi_dispa.h", "Make.glkterm"
+    if build.with?("cheapglk") && build.with?("glkterm")
+      odie "Options --with-cheapglk and --with-glkterm are mutually exclusive."
     end
 
-    inreplace "Makefile", "GLKINCLUDEDIR = ../cheapglk", "GLKINCLUDEDIR = #{glkterm_include}"
-    inreplace "Makefile", "GLKLIBDIR = ../cheapglk", "GLKLIBDIR = #{glkterm_lib}"
-    inreplace "Makefile", "Make.cheapglk", "Make.glkterm"
+    if build.with? "cheapglk"
+      glk = Formula["cheapglk"]
+    elsif build.with? "glkterm"
+      glk = Formula["glkterm"]
+    else
+      glk = Formula["glktermw"]
+    end
+
+    inreplace "Makefile", "GLKINCLUDEDIR = ../cheapglk", "GLKINCLUDEDIR = #{glk.include}"
+    inreplace "Makefile", "GLKLIBDIR = ../cheapglk", "GLKLIBDIR = #{glk.lib}"
+    inreplace "Makefile", "Make.cheapglk", "Make.#{glk.name}"
 
     system "make"
     bin.install "glulxe"
+  end
+
+  test do
+    if build.with? "cheapglk"
+      assert shell_output("#{bin}/glulxe").start_with? "Welcome to the Cheap Glk Implementation"
+    else
+      assert pipe_output("#{bin}/glulxe -v").start_with? "GlkTerm, library version"
+    end
   end
 end
