c5116f42b54565e7b8eb74d538f833bc43d9f1e6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-20 09:36:19 +0100

    glktermw: add 1.0.4 bottle.

--- glktermw.rb
+++ glktermw.rb
@@ -5,6 +5,13 @@ class Glktermw < Formula
   version "1.0.4"
   sha256 "5968630b45e2fd53de48424559e3579db0537c460f4dc2631f258e1c116eb4ea"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4fb944dd33eb8943ffa988e20001bde6c76969d8dab98ec82dfa9b12586b5081" => :sierra
+    sha256 "4cabfbfc3cf4f9946986424ca5741d3146c84e055d12418ef9c81f7795295f44" => :el_capitan
+    sha256 "1c2dc8d72af8e15f7c8b38935c4bc7abeff3328143a5b0ad66da88e682d6e8dd" => :yosemite
+  end
+
   keg_only "Conflicts with other Glk libraries"
 
   def install
