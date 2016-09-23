ce5d8f7a66c2fca6f3654ade99e51d57133d9359
Kyungdahm Yun <tomyun@gmail.com>
2016-09-22 17:22:10 -0700

    fs-uae 2.8.0
    Closes #678.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- fs-uae.rb
+++ fs-uae.rb
@@ -1,11 +1,10 @@
 class FsUae < Formula
   desc "Amiga emulator"
   homepage "http://fs-uae.net/"
-  revision 1
 
   stable do
-    url "http://fs-uae.net/fs-uae/stable/2.6.2/fs-uae-2.6.2.tar.gz"
-    sha256 "31328e8da0d31d85ce5ce46830698b83c0ce04dfe6b0f5df6fd3529dba3c0dd4"
+    url "https://fs-uae.net/stable/2.8.0/fs-uae-2.8.0.tar.gz"
+    sha256 "45d038125489e1fbc9094d411ba8413d8394f8b3ab8072baba391308ffa47bb3"
   end
 
   bottle do
@@ -13,12 +12,6 @@ class FsUae < Formula
     sha256 "3204920d6a626985b4241331cab489b41b9d15cbfb068e06373b7389a8e613e5" => :el_capitan
     sha256 "8ab012e2af806983f3cc4a17d872c014fc197b405b4407c6c8f1dd8f97db610e" => :yosemite
     sha256 "6651eefe49050d00499121e08e5fc8b076d43361f86f090fb1127842fb3a74d0" => :mavericks
-  end
-
-  devel do
-    url "http://fs-uae.net/fs-uae/devel/2.7.14dev/fs-uae-2.7.14dev.tar.gz"
-    version "2.7.14dev"
-    sha256 "9df7445bccef5255a66135f040dfbcac48b20df79f09daced166c8771428da83"
   end
 
   head do
