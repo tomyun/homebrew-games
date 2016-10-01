8d9486f534a6e0bd8483200ad6945389f5931af4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 12:18:00 +0100

    reminiscence: add 0.3.2 bottle.

--- reminiscence.rb
+++ reminiscence.rb
@@ -4,6 +4,13 @@ class Reminiscence < Formula
   url "http://cyxdown.free.fr/reminiscence/REminiscence-0.3.2.tar.bz2"
   sha256 "063a1d9bb61a91ffe7de69516e48164a1d4d5d240747968bed4fd292d5df546f"
 
+  bottle do
+    cellar :any
+    sha256 "a11a7b62cbbcd30e24742dc4d1ef881f2f5cedc5a6f61bb18fe6d5092e7c7986" => :sierra
+    sha256 "de842e520b31200e696e0139ccb213e1a5704b65529e53af8ea52a8b0c1fdcd2" => :el_capitan
+    sha256 "6f664f7cdcf1d5b90f2c82c30f13cdf6960ebd2e83882d4d05cdac0bac1966b0" => :yosemite
+  end
+
   depends_on "libmodplug"
   depends_on "sdl2"
 
