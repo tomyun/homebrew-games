04115bb18bb0c604a7b11c179041511d6196dc5d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-02 07:55:06 +0100

    rawgl: add 0.2.1 bottle.

--- rawgl.rb
+++ rawgl.rb
@@ -5,6 +5,12 @@ class Rawgl < Formula
   sha256 "da00d4abbc266e94c3755c2fd214953c48698826011b1d4afbebb99396240073"
   head "https://github.com/cyxx/rawgl.git"
 
+  bottle do
+    cellar :any
+    sha256 "f3b4affdbc9313bdb206923ee1d12e509a9acf539d25818832270cbc1c4c16a8" => :sierra
+    sha256 "ed075b6796ac69e2912ee05404672f70e611276a9d884f878e8285d7a14b6214" => :yosemite
+  end
+
   depends_on "sdl2"
   depends_on "sdl2_mixer"
 
