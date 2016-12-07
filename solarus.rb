83e75968ffb6609249a2e9eaf651826cfae3386b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-07 11:45:25 +0000

    solarus: update 1.5.1 bottle.

--- solarus.rb
+++ solarus.rb
@@ -7,9 +7,9 @@ class Solarus < Formula
 
   bottle do
     cellar :any
-    sha256 "d589825751eb7dbf9a6d19cad7c04e4326d9c213c50e77aeb9964ae7a2e3ec8f" => :el_capitan
-    sha256 "119fcef5923a8062fcadaddced499f33fb29c9c2c42c6cdfbce8ddf52ad926f5" => :yosemite
-    sha256 "144e2504a692e5784c1ee4c270b8a88cf6fce766fecf6072a7cd1fd7ec3aaa90" => :mavericks
+    sha256 "21588d712ee8bfa0047baecac8fb80157fe207f204e1900be3e0d94c28886032" => :sierra
+    sha256 "93dec2fb97684a7df689af1a526ab40134d0d42cc58cb1ca0ce5a92ecfa411e6" => :el_capitan
+    sha256 "66a5b11989a4ca95b66b8f504179df20995e0f822225cc7a58adada6fb5f4385" => :yosemite
   end
 
   depends_on "cmake" => :build
