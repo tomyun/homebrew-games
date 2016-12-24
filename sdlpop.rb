b4976026dc872c0559f062610b4b65ae3db2b5e4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-24 20:38:11 +0000

    sdlpop: add 1.16 bottle.

--- sdlpop.rb
+++ sdlpop.rb
@@ -4,6 +4,13 @@ class Sdlpop < Formula
   url "https://github.com/NagyD/SDLPoP/archive/v1.16.tar.gz"
   sha256 "4198eecdb2c4fed8f609af810962c943572df83da99c571146cee1596e7ee55b"
 
+  bottle do
+    cellar :any
+    sha256 "b713faadd8a78caa632fe8a6c123ffac2e90a39911dd0dab32ceeca497e12496" => :sierra
+    sha256 "a16ea419116ba30407c9129db7bc9260dfa20e1e812c0b860665737b1f9c59be" => :el_capitan
+    sha256 "08a0057a866f3cdaff9dcc1c2591d340eaad77a5d8f49002be3bd18d096b7da6" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "sdl2"
   depends_on "sdl2_image"
